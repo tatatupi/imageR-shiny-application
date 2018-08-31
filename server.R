75df9d844b4503eeec3cdf4bb18b9ac3299b53f5
Taiguara <taiguara@worldsense.com>
2018-08-31 12:17:30 -0300

    image segmentation with imageR

--- server.R
+++ server.R
@@ -9,11 +9,12 @@
 
 library(shiny)
 library(imager)
+library(dplyr)
+library(png)
+
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-  
-   
   # output$distPlot <- renderPlot({
   #   
   #   # generate bins based on input$bins from ui.R
@@ -24,35 +25,73 @@ shinyServer(function(input, output) {
   #   hist(x, breaks = bins, col = 'darkgray', border = 'white')
   #   
   # })
-  filename <- normalizePath(file.path('./images',
-                                      paste('img')))
-  candy <- load.image(filename)
   
-  ind=0;
-    for i=1:size(img,1)
-    for j=1:size(img,2)
-    ind=ind+1;
-    x(ind,:)=img(i,j,:);
-    end
-  end
+  im <- load.example("coins")
   
-  
-  output$outImage <- renderImage({
+  output$preImage <- renderImage({
     # Return a list containing the filename and alt text
+    imOut <- t(coins[,,1,1])
+    outfile <- tempfile(fileext = ".png")
+    writePNG(imOut, target = outfile)
     
-    m <- 2
-    p <- input$bins
-    eps <- 0.01
-    N <- 10000
-    
-    n = dim(candy)[4]
-    
-    
-    
-    list(src = filename,
-         alt = paste("Number of segments", input$bins))
-    
+    list(src = outfile,
+         contentType = "image/png",
+         alt = "This is alternate text")
   }, deleteFile = FALSE)
+  
+  
+  output$thresh <- renderImage({
+    
+    d <- as.data.frame(im)
+    ##Subsamble, fit a linear model
+    m <- sample_n(d,input$samples) %>% lm(value ~ x*y,data=.) 
+    ##Correct by removing the trend
+    im.c <- im-predict(m,d)
+    out <- threshold(im.c)
+    out <- clean(out,3) %>% imager::fill(7)
+    plot(im,main="Thresholding")
+    highlight(out)
+    
+    dev.copy(png,'thresh.png')
+    dev.off()
+    
+    list(src = "thresh.png",
+         contentType = "image/png",
+         alt = "This is alternate text")
+  }, deleteFile = TRUE)
+  
+  output$water <- renderImage({
+    
+    bgPercent <- paste(toString(input$bgValue),"%",sep="")
+    fgPercent <- paste(toString(100-input$bgValue),"%",sep="")
+    
+    bg <- (!threshold(im.c,bgPercent))
+    fg <- (threshold(im.c,fgPercent))
+    imlist(fg,bg) %>% plot(layout="row")
+    #Build a seed image where fg pixels have value 2, bg 1, and the rest are 0
+    seed <- bg+2*fg
+    
+    edges <- imgradient(im,"xy") %>% enorm
+    p <- 1/(1+edges)
+    
+    ws <- (watershed(seed,p)==1)
+    
+    ws <- bucketfill(ws,1,1,color=2) %>% {!( . == 2) }
+    
+    plot(im,main="Watershed")
+    out2 <- clean(ws,5)
+    highlight(out2,col="green")
+    
+    dev.copy(png,'water.png')
+    dev.off()
+    
+    list(src = "water.png",
+         contentType = "image/png",
+         alt = "This is alternate text")
+  }, deleteFile = TRUE)
+  
+  
+  
   
   # 
   # output$outImage <- renderImage({
--- ui.R
+++ ui.R
@@ -13,21 +13,45 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Taiguara Tupinambás, Data Product CP, August 30, 2018"),
   
   # Sidebar with a slider input for number of bins 
   sidebarLayout(
     sidebarPanel(
-       sliderInput("bins",
-                   "Number of centers:",
-                   min = 2,
-                   max = 6,
-                   value = 6)
+      numericInput("samples",
+                   label="Number of Samples", 
+                   min = 1, 
+                   max = 1000000, step = 1,
+                   value = 1),
+       sliderInput("bgValue",
+                   label="Background Pixels %", 
+                   min = 1, 
+                   max = 100, post  = " %", 
+                   value = 50),
+       submitButton("Submit")
     ),
     
     # Show a plot of the generated distribution
-    mainPanel(
-      imageOutput("outImage")
+    
+    mainPanel(h1("Image Segmentation"),
+              tabsetPanel(type="tabs",
+                          tabPanel("Instructions and Motivation",
+                                   h2("Motivation"),
+                                   p("Image segmentation methods are important to break down an image into meaningful regions."),
+                                   p("We will use as an example an image of coins. We wish to determine the regions where the coins appear"),
+                                   imageOutput("preImage"),
+                                   h2("Instructions"),
+                                   p("Two methods were applied: a simple thresholding method with linear model; and a watershed method. In this app you can change parameters for each method and check the results for both of them"),
+                                   tags$ol(
+                                     tags$li("Thresholding and linear model: You can selec the amount of samples for the linear model. The higher the better"), 
+                                     tags$li("Watershed: you can select the percentage of the background pixels. A small value is ideal, but not too small.")
+                                   ),
+                                   p("Go the results page and check the default results. They don't look so good"),
+                                   p("Try selecting a higher number of samples and choosing a background percentage between 5 and 20%")),
+                          tabPanel("Results Comparison",
+                                   imageOutput("thresh"),
+                                   imageOutput("water"))
+                          )
     )
   )
 ))
